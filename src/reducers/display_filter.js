6b5deb7702bd01dfdffc80b74038c83906aa6270
Tonnam Balankura <tonnam.balankura@gmail.com>
2017-03-28 07:20:33 -0700

    lint display_filter.js

--- src/reducers/display_filter.js
+++ src/reducers/display_filter.js
@@ -1,12 +1,12 @@
-import * as types from '../actions/types';
+import * as types from '../actions/types'
 
 const displayFilter = (state = 'all', action) => {
   switch (action.type) {
     case types.DISPLAY_CATEGORY:
-      return action.category;
+      return action.category
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default displayFilter;
+export default displayFilter
