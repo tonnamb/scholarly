9edc4dc1af4c5e70f10c05936ccefa2a8ec282ba
Tonnam Balankura <tonnam.balankura@gmail.com>
2017-03-28 07:05:45 -0700

    lint query.js

--- src/reducers/query.js
+++ src/reducers/query.js
@@ -1,12 +1,12 @@
-import * as types from '../actions/types';
+import * as types from '../actions/types'
 
 const query = (state = null, action) => {
   switch (action.type) {
     case types.FETCH_RESULTS:
-      return action.payload.data.message.query['search-terms'];
+      return action.payload.data.message.query['search-terms']
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default query;
+export default query
