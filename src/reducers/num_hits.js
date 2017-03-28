3092dfa614faba97d9f5efb6b58e0fff74368551
Tonnam Balankura <tonnam.balankura@gmail.com>
2017-03-28 06:49:20 -0700

    lint num_hits.js

--- src/reducers/num_hits.js
+++ src/reducers/num_hits.js
@@ -1,12 +1,12 @@
-import * as types from '../actions/types';
+import * as types from '../actions/types'
 
 const numHits = (state = null, action) => {
   switch (action.type) {
     case types.FETCH_RESULTS:
-      return action.payload.data.message['total-results'];
+      return action.payload.data.message['total-results']
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default numHits;
+export default numHits
