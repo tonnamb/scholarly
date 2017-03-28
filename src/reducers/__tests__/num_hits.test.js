13eea752e5fe6e093f2e09b93ea5cce314297972
Tonnam Balankura <tonnam.balankura@gmail.com>
2017-03-28 07:00:41 -0700

    add reducer test for num_hits.js

---
+++ src/reducers/__tests__/num_hits.test.js
@@ -0,0 +1,25 @@
+import numHits from '../num_hits'
+import * as types from '../../actions/types'
+
+it('handles action with unknown type', () => {
+  expect(
+    numHits(undefined, { type: 'some unknown type' })
+  ).toEqual(
+    null
+  )
+})
+
+it('keeps track of number of search hits', () => {
+  const mockPayload = {
+    "data": {
+      "message": {
+        "total-results": 156683
+      }
+    }
+  }
+  expect(
+    numHits(null, { type: types.FETCH_RESULTS, payload: mockPayload })
+  ).toEqual(
+    156683
+  )
+})
