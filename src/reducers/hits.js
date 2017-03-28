4b32b868e6cc4a9205bf3642489d848ccf5506c4
Tonnam Balankura <tonnam.balankura@gmail.com>
2017-03-28 07:13:25 -0700

    lint hits.js

--- src/reducers/hits.js
+++ src/reducers/hits.js
@@ -1,12 +1,12 @@
-import * as types from '../actions/types';
+import * as types from '../actions/types'
 
 const hits = (state = [], action) => {
   switch (action.type) {
     case types.FETCH_RESULTS:
-      return action.payload.data.message.items;
+      return action.payload.data.message.items
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default hits;
+export default hits
