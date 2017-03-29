9e37291f5f342097b1ba3b17770b9597c9b95151
Tonnam Balankura <tonnam.balankura@gmail.com>
2017-03-29 08:51:40 -0700

    lint classified

--- src/reducers/classified.js
+++ src/reducers/classified.js
@@ -1,23 +1,23 @@
-import * as types from '../actions/types';
+import * as types from '../actions/types'
 
-function classifyItem(data, classifier) {
-  const title=(data.title[0] ? data.title[0] : '__title_not_present__');
-  return classifier.categorize(title);
+export function classifyItem (data, classifier) {
+  const title = (data.title[0] ? data.title[0] : '__title_not_present__')
+  return classifier.categorize(title)
 }
 
 const classifiedResults = (state = [], action) => {
   switch (action.type) {
     case types.CLASSIFY_RESULTS:
-      const items = action.payload.data.message.items;
-      const itemsLength = items.length;
+      const items = action.payload.data.message.items
+      const itemsLength = items.length
       const newState = []
-      for (let i=0; i<itemsLength; i++) {
-        newState.push({ ...items[i], classification: classifyItem(items[i], action.classifier) });
+      for (let i = 0; i < itemsLength; i++) {
+        newState.push({ ...items[i], classification: classifyItem(items[i], action.classifier) })
       }
-      return newState;
+      return newState
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default classifiedResults;
+export default classifiedResults
