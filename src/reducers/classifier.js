291d07261adf4810d1699ef025c3787f038ffa23
Tonnam Balankura <tonnam.balankura@gmail.com>
2017-03-29 08:37:51 -0700

    lint classifier.js

--- src/reducers/classifier.js
+++ src/reducers/classifier.js
@@ -1,18 +1,18 @@
-import * as types from '../actions/types';
-import Bayes from 'bayes';
+import * as types from '../actions/types'
+import Bayes from 'bayes'
 
-const defaultClassifier = Bayes();
+const defaultClassifier = Bayes()
 defaultClassifier.learn('', 'cannot classify')
 
 const classifier = (state = defaultClassifier, action) => {
   switch (action.type) {
     case types.TRAIN_CLASSIFIER:
-      const stateJson = state.toJson();
-      const newState = Bayes.fromJson(stateJson).learn(action.text, action.category);
-      return newState;
+      const stateJson = state.toJson()
+      const newState = Bayes.fromJson(stateJson).learn(action.text, action.category)
+      return newState
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default classifier;
+export default classifier
